06ad81d1a23d154c227c5979e131e49a52b03c19
Froyzend <froyzend1993@gmail.com>
2024-10-12 17:22:42 +0200

    update css/html

--- src/js/faq.js
+++ src/js/faq.js
@@ -5,3 +5,17 @@ new Accordion('.accordion-container', {
   openOnInit: [0],
   collapse: false,
 });
+
+const icon = document.querySelector('.ac-trigger');
+
+icon.addEventListener('click', function () {
+  icon.classList.toggle('icon-flipped');
+
+  const content = document.querySelector('.content');
+
+  if (icon.classList.contains('icon-flipped')) {
+    content.style.display = 'block';
+  } else {
+    content.style.display = 'none';
+  }
+});
