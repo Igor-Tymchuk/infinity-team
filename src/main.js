e25b74ff34650bac09c1e317fb46cb51739b5cd2
Anastasiia Bulba <fedinanastya@gmail.com>
2024-10-13 19:19:27 +0300

    js start

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import './js/smooth-scroll';
 import './js/modal-menu';
 import './js/my-projects';
 import './js/faq';
-import './js/reviews-pixabay-api';
-import './js/reviews-render-functions';
-import './js/reviews-main';
-
-
+// import './js/reviews-pixabay-api';
+// import './js/reviews-render-functions';
+// import './js/reviews-main';
+import './js/work-together';
+import './js/work-together-api';
