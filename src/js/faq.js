a804caae2fb22046f00724d5f667c5b1f2420419
Froyzend <froyzend1993@gmail.com>
2024-10-13 09:27:22 +0200

    commit

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,37 +1,10 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container'); /*
-{
+new Accordion('.faq-ac-container', {
   openOnInit: [0],
   collapse: false,
+  elementClass: 'faq-ac',
+  triggerClass: 'faq-ac-trigger',
+  panelClass: 'faq-ac-panel',
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-  const accordionItems = document.querySelectorAll('.ac');
-
-  accordionItems.forEach(item => {
-    const trigger = item.querySelector('.ac-trigger');
-    const panel = item.querySelector('.ac-panel');
-
-    trigger.addEventListener('click', function () {
-      const isOpen = item.classList.contains('active');
-      accordionItems.forEach(accItem => {
-        if (accItem !== item) {
-          accItem.classList.remove('active');
-          accItem.querySelector('.ac-panel').style.maxHeight = null;
-          accItem.querySelector('.ac-trigger').style.transform = 'rotate(0deg)';
-        }
-      });
-      if (isOpen) {
-        item.classList.remove('active');
-        panel.style.maxHeight = null;
-        trigger.style.transform = 'rotate(0deg)';
-      } else {
-        item.classList.add('active');
-        panel.style.maxHeight = panel.scrollHeight + 'px';
-        trigger.style.transform = 'rotate(180deg)';
-      }
-    });
-  });
-});*/
