a056df7f0e914903ea1e7e95177d5618cd5007ba
YuriiM-Owl <129115158+YuriiM-Owl@users.noreply.github.com>
2024-10-14 13:33:04 +0300

    review-styles seems to be done?

--- src/js/reviews-render-functions.js
+++ src/js/reviews-render-functions.js
@@ -1,28 +1,30 @@
 import { list } from './reviews-main';
 
 export function renderReviews(posts) {
-    const markup = posts
-        .map(post => {
-            const { review, avatar_url, author } = post;
-            return `
+  const markup = posts
+    .map(post => {
+      const { review, avatar_url, author } = post;
+      return `
             <li class="reviews-item swiper-slide">
-              <p class="reviews-text">
-                ${review}
-              </p>
-              <div class="reviews-container">
-                <div class="reviews-wrap-img">
-                  <img src="${avatar_url}" alt="avatar" class="reviews-img" />
+              <div class="slide-box">
+                <p class="review-text">
+                  ${review}
+                </p>
+                <div class="review-author-box">
+                  <div class="review-author-img-box">
+                    <img src="${avatar_url}" alt="author-avatar" class="review-author-img" width="40" height="40"/>
+                  </div>
+                  <p class="review-author-name">${author}</p>
                 </div>
-                <p class="reviews-name">${author}</p>
               </div>
             </li>`;
-        })
-        .join('');
-    list.insertAdjacentHTML('beforeend', markup);
+    })
+    .join('');
+  list.insertAdjacentHTML('beforeend', markup);
 }
 
 export function errorPost() {
-    const markup =
-        '<li class="reviews-item"><p class="reviews-name">Not found</p></li>';
-    list.insertAdjacentHTML('beforeend', markup);
+  const markup =
+    '<li class="reviews-item"><p class="reviews-name">Not found</p></li>';
+  list.insertAdjacentHTML('beforeend', markup);
 }
